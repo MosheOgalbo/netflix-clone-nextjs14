b8b7d6b76fff296aeb42ead75ab559a0a3a94d6c
MosheOgalbo <65408488+MosheOgalbo@users.noreply.github.com>
2024-09-09 15:33:46 +0300

    add prisma

--- app/utils/auth.tsx
+++ app/utils/auth.tsx
@@ -1,12 +1,12 @@
 import type { NextAuthOptions } from "next-auth";
-//import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./db";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 
 export const authOptions = {
-  //adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
